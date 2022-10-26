dbd8444159eeb5a04bef03c4f572b847fa382487
kerenAdini <116174675+kerenAdini@users.noreply.github.com>
2022-10-26 10:28:55 +0300

    Add files via upload

--- consts.py
+++ consts.py
@@ -5,3 +5,4 @@ CENTER_HOSPITALS = ["Sheba", "Ichilov"]
 NORTH_HOSPITALS = ["Ziv", "Rambam"]
 HOSPITAL_NUM = len(SOUTH_HOSPITALS) + len(CENTER_HOSPITALS) + len(NORTH_HOSPITALS)
 ALL_HOSPITALS = ["Ziv", "Rambam", "Sheba", "Ichilov", "Soroka", "Brazilai"]
+AREA_LIST = [SOUTH_HOSPITALS, CENTER_HOSPITALS, NORTH_HOSPITALS]
